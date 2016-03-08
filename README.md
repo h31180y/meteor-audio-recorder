43875a8b3f7677b68c72768e34a76409cba62882
Maxence Cornet <maxence.cornet20@gmail.com>
2016-03-08 17:17:36 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# audio-recorder
+
+Record audio input from the user microphone, export to .wav or ArrayBuffer
+
+## Methods
+
+
+
+AudioRecorder has 2 public methods : ```startRecording()``` and ```stopRecording(format, filename, callback)```.
+
+startRecording is synchronous and doesnt take any arguments.
+stopRecording format argument accept 2 formats : 'wav' or 'Uint8Array', the first one must be called synchonously, the second one is async.
+ 
+
+
+## Usage
+```javascript
+var audioRecorder = new AudioRecorder();
+
+audioRecorder.startRecording();
+
+//Sync usage with .wav
+audioRecorder.stopRecording('wav', 'wavFile');
+
+// Async usage with ArrayBuffer
+audioRecorder.stopRecording('Uint8Array', 'ArrayBufferFile', function (error, result) {
+    // result contains the audio file input Array buffer
+});
+```
+
+
+```javascript var audioRecorder = new AudioRecorder(); ```
+
+
+
---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+    name: 'maxencecornet:audio-recorder',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'Record audio input from the user microphone, export to .wav or ArrayBuffer',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.2.1');
+    api.use(['ecmascript', 'check']);
+
+    api.addFiles(['lib/client/compatibility/recorder.js', 'lib/client/audio-recorder.js'], ['client']);
+    api.export('AudioRecorder', 'client');
+});
+
+Package.onTest(function (api) {
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('maxencecornet:audio-recorder');
+});
